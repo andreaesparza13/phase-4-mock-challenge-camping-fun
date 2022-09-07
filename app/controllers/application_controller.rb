1ed88ef41e28568a9eb03219365a975fe87d81b9
Andrea Esparza <andreaesparza13@users.noreply.github.com>
2022-09-07 15:22:14 -0700

    crushed it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+  private
+
+  def record_invalid(err)
+    render json: { error: err.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def record_not_found(err)
+    render json: { error: err.message }, status: :not_found
+  end
+
 end
---
+++ app/serializers/show_camper_activities_serializer.rb
@@ -0,0 +1,5 @@
+class ShowCamperActivitiesSerializer < ActiveModel::Serializer
+  attributes :id, :name, :age
+
+  has_many :activities
+end
